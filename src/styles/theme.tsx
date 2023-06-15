d2a8aa77b512e268c54838913f8d2db7ba4dd7eb
M Syahrul Surya Putra <65522975+msyahrulsp@users.noreply.github.com>
2023-06-16 05:31:53 +0700

    feat: add font (#13)

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -2,6 +2,10 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   // TODO: add font + colors
+  fonts: {
+    heading: 'Bodwars',
+    body: 'SomarRounded-Regular'
+  },
   styles: {
     global: {
       body: {
