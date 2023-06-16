14b4f61f2fa7c3d10b88abd44f1b1583d0a3aca6
Yakobus Iryanto Prasethio <82079465+YakobusIP@users.noreply.github.com>
2023-06-16 16:12:24 +0700

    Feat/assignment mentee function (#18)
    * feat: assignment mentee functions
    
    * feat: add error handling on student fetching

--- src/server/api/routers/studentAssignment.ts
+++ src/server/api/routers/studentAssignment.ts
@@ -1,36 +1,63 @@
-import { z } from "zod";
-import {
-  createTRPCRouter,
-  studentProcedure,
-} from "~/server/api/trpc";
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+import { createTRPCRouter, studentProcedure } from '~/server/api/trpc';
 
 export const studentAssignmentRouter = createTRPCRouter({
   getDeskripsiTugas: studentProcedure
-    .input(z.object({
-      namaTugas: z.string()
-    }))
+    .input(
+      z.object({
+        namaTugas: z.string()
+      })
+    )
     .query(async ({ ctx, input }) => {
-      // TODO
-      // get deskripsi tugas by nama tugas
-      return input.namaTugas
+      return await ctx.prisma.assignment.findMany({
+        where: {
+          title: input.namaTugas
+        }
+      });
     }),
 
-  getListNamaTugas: studentProcedure
-    .query(async ({ ctx }) => {
-      // TODO
-      // get list semua nama tugas
-      return []
-    }),
+  getListNamaTugas: studentProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.assignment.findMany({
+      select: {
+        title: true
+      }
+    });
+  }),
 
   updateSubmission: studentProcedure
-    .input(z.object({
-      assignmentId: z.string(),
-      userId: z.string(),
-      fileUrl: z.string()
-    }))
+    .input(
+      z.object({
+        assignmentId: z.string(),
+        userId: z.string(),
+        fileUrl: z.string()
+      })
+    )
     .mutation(async ({ ctx, input }) => {
-      // TODO
-      // update status submisi dari tugas based on input
-      return null
+      const student = await ctx.prisma.student.findFirst({
+        where: {
+          userId: input.userId
+        },
+        select: {
+          id: true
+        }
+      });
+
+      if (!student) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: "Student doesn't exist"
+        });
+      }
+
+      await ctx.prisma.assignmentSubmission.updateMany({
+        where: {
+          studentId: student.id,
+          assignmentId: input.assignmentId
+        },
+        data: {
+          filePath: input.fileUrl
+        }
+      });
     })
 });
