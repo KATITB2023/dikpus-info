9a3721d80bc7d377df7a1e1714c724a6d8f47fdf
Yakobus Iryanto Prasethio <82079465+YakobusIP@users.noreply.github.com>
2023-06-17 23:46:39 +0700

    Fix/attendance function (#40)
    * refactor: add nullable on filepath
    
    * feat: generate rows for attendance and assignment
    
    * feat: revamp attendance system
    
    * fix: fix merging issue on dates

--- src/server/api/routers/attendance.ts
+++ src/server/api/routers/attendance.ts
@@ -8,9 +8,34 @@ import {
 } from '~/server/api/trpc';
 
 export const attendanceRouter = createTRPCRouter({
-  getEvents: studentProcedure.query(async ({ ctx }) => {
-    return ctx.prisma.event.findMany();
-  }),
+  getEvents: studentProcedure
+    .input(z.object({ userId: z.string().uuid() }))
+    .query(async ({ ctx, input }) => {
+      const student = await ctx.prisma.student.findFirst({
+        where: {
+          userId: input.userId
+        }
+      });
+
+      if (!student) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Student not found'
+        });
+      }
+
+      const attendance = await ctx.prisma.attendance.findMany({
+        where: {
+          studentId: student.id
+        },
+        select: {
+          status: true,
+          event: true
+        }
+      });
+
+      return attendance;
+    }),
 
   setAttendance: studentProcedure
     .input(z.object({ userId: z.string().uuid(), eventId: z.string().uuid() }))
@@ -44,14 +69,14 @@ export const attendanceRouter = createTRPCRouter({
         });
       }
 
-      const attendance = await ctx.prisma.attendance.findMany({
+      const attendance = await ctx.prisma.attendance.findFirst({
         where: {
           studentId: student.id,
           eventId: input.eventId
         }
       });
 
-      if (attendance.length > 0) {
+      if (attendance && attendance.status === 'HADIR') {
         throw new TRPCError({
           code: 'BAD_REQUEST',
           message: 'Attendance already set'
@@ -60,12 +85,14 @@ export const attendanceRouter = createTRPCRouter({
 
       const currentTime = new Date(Date.now());
       if (currentTime > event.startTime && currentTime < event.endTime) {
-        await ctx.prisma.attendance.create({
-          data: {
-            date: currentTime,
-            status: 'HADIR',
+        await ctx.prisma.attendance.updateMany({
+          where: {
             studentId: student.id,
             eventId: input.eventId
+          },
+          data: {
+            date: currentTime,
+            status: 'HADIR'
           }
         });
 
@@ -93,9 +120,6 @@ export const attendanceRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      // TODO
-      // get absensi
-
       // get mentor_id
       const mentor = await ctx.prisma.mentor.findFirst({
         where: {
@@ -114,44 +138,74 @@ export const attendanceRouter = createTRPCRouter({
         });
       }
 
+      // get mentor group
+      const mentorGroups = await ctx.prisma.mentorGroup.findMany({
+        where: {
+          mentorId: mentor.id
+        },
+        select: {
+          groupId: true
+        }
+      });
+
+      if (mentorGroups.length === 0) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Mentor group not found'
+        });
+      }
+
+      const groups = await ctx.prisma.group.findMany({
+        where: {
+          id: {
+            in: mentorGroups.map((mentorGroup) => mentorGroup.groupId)
+          }
+        },
+        select: {
+          id: true,
+          group: true
+        }
+      });
+
+      if (groups.length === 0) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Group not found'
+        });
+      }
+
       // get all student_id
       const students = await ctx.prisma.student.findMany({
         where: {
-          mentorId: mentor.id
+          groupId: {
+            in: groups.map((group) => group.id)
+          }
         },
         select: {
           id: true
         }
       });
 
-      // get all attendance per Event
-      const attendances = await ctx.prisma.event.findMany({
-        where: {
-          attendances: {
-            some: {
-              id: {
-                in: students.map((student) => student.id)
-              }
-            }
-          }
-        },
+      const studentIds = students.map((student) => student.id);
+
+      const events = await ctx.prisma.event.findMany({
         select: {
+          title: true,
+          startTime: true,
+          endTime: true,
           attendances: {
             select: {
               id: true,
-              date: true,
               status: true,
               studentId: true,
               eventId: true,
-              createdAt: true,
-              updatedAt: true,
-              reason: {
+              student: {
                 select: {
-                  id: true,
-                  reason: true,
-                  attendanceId: true,
-                  createdAt: true,
-                  updatedAt: true
+                  group: {
+                    select: {
+                      group: true
+                    }
+                  }
                 }
               }
             }
@@ -159,15 +213,22 @@ export const attendanceRouter = createTRPCRouter({
         }
       });
 
-      return attendances.map((attendance) => attendance.attendances).flat();
+      return {
+        event: events.map((event) => {
+          return {
+            ...event,
+            attendances: event.attendances.filter((attendance) =>
+              studentIds.includes(attendance.studentId)
+            )
+          };
+        })
+      };
     }),
 
-  getDateList: mentorProcedure.query(async ({ ctx }) => {
-    // TODO
-    // get list tanggal absensi
-    return await ctx.prisma.attendance.findMany({
+  getEventList: mentorProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.event.findMany({
       select: {
-        date: true
+        title: true
       }
     });
   }),
@@ -175,8 +236,7 @@ export const attendanceRouter = createTRPCRouter({
   editAttendance: mentorProcedure
     .input(
       z.object({
-        mentorId: z.string().uuid(),
-        studentId: z.string().uuid(),
+        attendanceId: z.string().uuid(),
         kehadiran: z.nativeEnum(AttendanceStatus)
       })
     )
@@ -185,7 +245,7 @@ export const attendanceRouter = createTRPCRouter({
       // edit absensi
       await ctx.prisma.attendance.update({
         where: {
-          id: input.studentId
+          id: input.attendanceId
         },
         data: {
           status: input.kehadiran
--- src/server/api/routers/utility.ts
+++ src/server/api/routers/utility.ts
@@ -1,5 +1,11 @@
+import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 import { createTRPCRouter, mentorProcedure } from '~/server/api/trpc';
+
+const isValidDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return !isNaN(Number(date));
+};
 
 export const utilityRouter = createTRPCRouter({
   addEvent: mentorProcedure
@@ -7,18 +13,42 @@ export const utilityRouter = createTRPCRouter({
       z.object({
         title: z.string(),
         materialPath: z.string(),
-        startTime: z.date(),
-        endTime: z.date()
+        startTime: z.string(),
+        endTime: z.string()
       })
     )
     .mutation(async ({ ctx, input }) => {
-      await ctx.prisma.event.create({
+      if (!isValidDate(input.startTime) || !isValidDate(input.endTime)) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Incorrect date format'
+        });
+      }
+
+      const event = await ctx.prisma.event.create({
         data: {
           title: input.title,
           materialPath: input.materialPath,
-          startTime: input.startTime,
-          endTime: input.endTime
+          startTime: new Date(input.startTime),
+          endTime: new Date(input.endTime)
         }
+      });
+
+      const students = await ctx.prisma.student.findMany({
+        select: {
+          id: true
+        }
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      students.forEach(async (student) => {
+        await ctx.prisma.attendance.create({
+          data: {
+            date: new Date(Date.now()),
+            studentId: student.id,
+            eventId: event.id
+          }
+        });
       });
 
       return {
@@ -31,16 +61,39 @@ export const utilityRouter = createTRPCRouter({
       z.object({
         title: z.string(),
         description: z.string(),
-        deadline: z.date()
+        deadline: z.string()
       })
     )
     .mutation(async ({ ctx, input }) => {
-      await ctx.prisma.assignment.create({
+      if (!isValidDate(input.deadline)) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Incorrect date format'
+        });
+      }
+
+      const assigment = await ctx.prisma.assignment.create({
         data: {
           title: input.title,
           description: input.description,
-          deadline: input.deadline
+          deadline: new Date(input.deadline)
         }
+      });
+
+      const students = await ctx.prisma.student.findMany({
+        select: {
+          id: true
+        }
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      students.forEach(async (student) => {
+        await ctx.prisma.assignmentSubmission.create({
+          data: {
+            studentId: student.id,
+            assignmentId: assigment.id
+          }
+        });
       });
 
       return {
