d84d87c5f8b76a510c5f4428d82bc436cf3a8f71
msyahrulsp <mssp892@gmail.com>
2023-06-18 04:45:25 +0700

    chore: add fixed routing for fe

---
+++ src/pages/assignment.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import PageLayout from '~/layout';
+import { useSession } from 'next-auth/react';
+import { Box } from '@chakra-ui/react';
+import { UserRole } from '@prisma/client';
+import { RedirectLogin } from '~/component/RedirectLogin';
+
+const Assignment = () => {
+  const { data: session, status } = useSession();
+
+  if (!session && status !== 'loading') {
+    // pake useRouter next gabisa idk why
+    return <RedirectLogin />;
+  }
+
+  const role = session?.user.role;
+
+  return (
+    <PageLayout title='Absen'>
+      {role === UserRole.MENTOR ? (
+        <Box>Assignment Mentor</Box>
+      ) : (
+        <Box>Assignment Mentee</Box>
+      )}
+    </PageLayout>
+  );
+};
+
+export default Assignment;
