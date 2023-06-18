ff68fb0321304a1e70c65395943b0a23413263e6
jasonk19 <13520080@std.stei.itb.ac.id>
2023-06-18 20:19:56 +0700

    refactor: get assignment result

--- src/server/api/routers/assignment.ts
+++ src/server/api/routers/assignment.ts
@@ -31,22 +31,102 @@ export const assignmentRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const submissionList = await ctx.prisma.assignmentSubmission.findMany({
+      // get mentorId
+      const mentor = await ctx.prisma.mentor.findFirst({
         where: {
-          assignment: {
-            is: {
-              title: input.namaTugas
-            }
+          userId: input.userId
+        },
+        select: {
+          id: true
+        }
+      });
+
+      // error handling (kalau gak ada ini yg students gak mau jalan)
+      if (!mentor) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Mentor not found"
+        });
+      }
+
+      // get mentor group
+      const mentorGroups = await ctx.prisma.mentorGroup.findMany({
+        where: {
+          mentorId: mentor.id
+        },
+        select: {
+          groupId: true
+        }
+      });
+
+      if (mentorGroups.length === 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Mentor group not found"
+        });
+      }
+
+      const groups = await ctx.prisma.group.findMany({
+        where: {
+          id: {
+            in: mentorGroups.map((mentorGroup) => mentorGroup.groupId)
+          }
+        },
+        select: {
+          id: true,
+          group: true
+        }
+      });
+
+      if (groups.length === 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Group not found"
+        });
+      }
+
+      // get all studentId
+      const students = await ctx.prisma.student.findMany({
+        where: {
+          groupId: {
+            in: groups.map((group) => group.id)
           },
-          student: {
-            is: {
-              id: input.studentId,
-              group: {
-                mentorGroup: {
-                  some: {
-                    mentor: {
-                      is: {
-                        userId: input.userId
+          id: input.studentId
+        },
+        select: {
+          id: true
+        }
+      });
+
+      const studentIds = students.map((student) => student.id);
+
+      const events = await ctx.prisma.event.findMany({
+        select: {
+          title: true,
+          startTime: true,
+          endTime: true,
+          attendances: {
+            select: {
+              id: true,
+              status: true,
+              studentId: true,
+              eventId: true,
+              student: {
+                select: {
+                  firstName: true,
+                  lastName: true,
+                  group: {
+                    select: {
+                      group: true
+                    }
+                  },
+                  submission: {
+                    select: {
+                      filePath: true,
+                      assignment: {
+                        select: {
+                          title: true,
+                        }
                       }
                     }
                   }
@@ -54,25 +134,29 @@ export const assignmentRouter = createTRPCRouter({
               }
             }
           }
-        },
-        include: {
-          student: {
-            select: {
-              id: true,
-              firstName: true,
-              lastName: true,
-              userId: true,
-              group: {
-                select: {
-                  id: true,
-                  group: true
+        }
+      })
+
+      return {
+        event: events.map((event) => {
+          return {
+            ...event,
+            attendances: event.attendances.filter((attendance) =>
+              studentIds.includes(attendance.studentId)
+            ).map((attendance) => {
+              return {
+                ...attendance,
+                student: {
+                  ...attendance.student,
+                  submission: input.namaTugas ? attendance.student.submission.filter((sub) => 
+                    sub.assignment.title === input.namaTugas
+                  ) : attendance.student.submission
                 }
               }
-            }
+            })
           }
-        }
-      });
-      return submissionList;
+        })
+      }
     }),
 
   getAssignmentNameList: protectedProcedure.query(async ({ ctx }) => {
