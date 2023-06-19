84bce7e312f6f742d8e28ee8ff94457ba7eea51a
Chiquita Ahsanunnisa <88751131+ashnchiquita@users.noreply.github.com>
2023-06-19 07:13:33 +0700

    feat/mentor side attendance page (#49)
    * feat: attendance page styling
    
    * feat: some logic & integrate with backend
    
    * feat: edit attendance
    
    * fix: type error & filter state error
    
    * refactor: mentor side attendance page
    
    * feat: get group list
    
    * chore: eslint stuff
    
    ---------
    
    Co-authored-by: msyahrulsp <mssp892@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import { type Session } from "next-auth";
-import { type AppType } from "next/app";
-import { SessionProvider } from "next-auth/react";
-import { ChakraProvider } from "@chakra-ui/react";
-import { api } from "~/utils/api";
-import theme from "~/styles/theme";
-import "~/styles/globals.css";
+import { type Session } from 'next-auth';
+import { type AppType } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { api } from '~/utils/api';
+import theme from '~/styles/theme';
+import '~/styles/globals.css';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
--- src/pages/attendance.tsx
+++ src/pages/attendance.tsx
@@ -1,20 +1,20 @@
-import { signIn, useSession } from "next-auth/react";
-import { type NextPage } from "next";
-import { Box } from "@chakra-ui/react";
-import { UserRole } from "@prisma/client";
-import { MenteeAttendance } from "~/component/attendance/MenteeAttendance";
-import PageLayout from "~/layout";
+import { signIn, useSession } from 'next-auth/react';
+import { type NextPage } from 'next';
+import { UserRole } from '@prisma/client';
+import { MenteeAttendance } from '~/component/attendance/MenteeAttendance';
+import { MentorAttendance } from '~/component/attendance/MentorAttendance';
+import PageLayout from '~/layout';
 
 const Attendance: NextPage = () => {
   const { data: session, status } = useSession();
 
-  if (status === "unauthenticated") return signIn();
+  if (status === 'unauthenticated') return signIn();
 
   const role = session?.user.role;
 
   return (
     <PageLayout title='Absen'>
-      {role === UserRole.MENTOR ? <Box>Mentor</Box> : <MenteeAttendance />}
+      {role === UserRole.MENTOR ? <MentorAttendance /> : <MenteeAttendance />}
     </PageLayout>
   );
 };
