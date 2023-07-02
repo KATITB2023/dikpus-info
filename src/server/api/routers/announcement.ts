5ae22823aa6c2ae111998c574c8e6a6db103443f
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-07-02 17:55:39 +0700

    feat: add accepted status

--- src/server/api/routers/announcement.ts
+++ src/server/api/routers/announcement.ts
@@ -43,7 +43,8 @@ export const announcementRouter = createTRPCRouter({
 
     if (!announcement) {
       return {
-        message: "Maaf, Anda tidak lolos proses recruitment."
+        message: "Maaf, Anda tidak lolos proses recruitment.",
+        accepted: false,
       }
     }
 
